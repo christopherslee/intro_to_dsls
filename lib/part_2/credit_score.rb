27167c3cb5656271c5ab5b252c680b3fb4969a2e
Christopher Lee <christopher.s.lee@gmail.com>
2013-08-05 21:43:57 -0400

    remove the answer, oops

--- lib/part_2/credit_score.rb
+++ lib/part_2/credit_score.rb
@@ -5,8 +5,5 @@ module CreditScore
     # TODO Part 2
     # create a consumer and use instance eval to execute the scenario
     # and then return the resulting score
-    consumer = Consumer.new
-    consumer.instance_eval(&block)
-    consumer.score
   end
 end
